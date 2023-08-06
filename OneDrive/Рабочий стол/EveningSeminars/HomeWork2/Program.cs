d678782566dc5b8803a3248118a5298b6749664e
Pavel Peredrij <peredrij.pavel19@gmail.com>
2023-08-06 07:21:53 +0300

    Added task 13

---
+++ OneDrive/Рабочий стол/EveningSeminars/HomeWork2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Task 10. Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+/*
+int CutNumber(int num)
+{
+    int pair = num / 10;
+    int tens = pair % 10;
+
+    int res = tens;
+    return res;
+}
+
+Console.Write("Input a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int newNumber = CutNumber(num);
+Console.WriteLine($"New version of {num} is {newNumber}");
+*/
+// Task 13. Напишите программу, которая выводит третью цифру заданного числа ( или -1, если третьей цифры нет).
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 100 && number < 1000)
+{
+    int num = number % 10;
+    Console.Write(num);
+}
+else if(number < 100)
+{
+    Console.Write("Третьей цифры нет.");
+}
+else if(number > 999)
+{
+    int num = number / 10;
+    int num1 = num % 10;
+    Console.Write(num1);
+}
