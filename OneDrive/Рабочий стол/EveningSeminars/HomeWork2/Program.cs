997b17dbf136ebaf6d50efd1e1f42cbfa5506dc9
Pavel Peredrij <peredrij.pavel19@gmail.com>
2023-08-06 08:00:53 +0300

    Added task 15

--- OneDrive/Рабочий стол/EveningSeminars/HomeWork2/Program.cs
+++ OneDrive/Рабочий стол/EveningSeminars/HomeWork2/Program.cs
@@ -17,7 +17,7 @@ int newNumber = CutNumber(num);
 Console.WriteLine($"New version of {num} is {newNumber}");
 */
 // Task 13. Напишите программу, которая выводит третью цифру заданного числа ( или -1, если третьей цифры нет).
-
+/*
 Console.Write("Input a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -35,4 +35,20 @@ else if(number > 999)
     int num = number / 10;
     int num1 = num % 10;
     Console.Write(num1);
+}
+*/
+
+// Task 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num >= 1 && num <=5)
+{
+    Console.Write("Weekdays");
+}
+else if(num == 6 || num == 7)
+{
+    Console.Write("Weekend");
 }
